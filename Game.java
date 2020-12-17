abe875e83dd745c5d9cfe0bb35e007b156b3a8ed
ngawang30 <ngawang30@gmail.com>
2020-12-17 01:55:43 -0500

    Anything

--- Game.java
+++ Game.java
@@ -1,5 +1,7 @@
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+	static Scanner sc = new Scanner(System.in);
 	String QUIT = "q";
 	String TAKE_3 = "p3";
 	String TAKE_2 = "p2";
@@ -43,10 +45,101 @@ public class Game {
 		boolean game_over = false;
 		boolean player1_turn = true;
 		boolean game_over_p = false;
+		Player p;		
+
+		while(!game_over){
+			screen();
+			while(sum_gems(player_1) > 10 || sum_gems(player_2) > 10){
+				if(!player1_turn) over_10_full(player_1);
+				else over_10_full(player_2);
+			}
+			p = player_1;
+			instructions(game_over_p, p, player1_turn);
+			player1_turn = !player1_turn;
+		}
+	}
+
+
+	public void instructions(boolean game_over_p, p, boolean player1_turn){
+		String player_move;
+		
+		if(player1_turn== true){
+			System.out.println("PLAYER 1, enter your next move: ");
+			p = player_1;
+		} else {
+			System.out.println("PLAYER 2, enter your next move: ");
+			p = player_2;
+		}
+
+
+		player_move = sc.next();
+
+		if(player_move == QUIT){
+			game_over_p = true;
+		} else if (player-move == TAKE_3) take_3_full(p);
+		else if(player_move == TAKE_2){
+			String gem;
+			gem = sc.next();
+			take_2(p,gem);
+		} else if(player_move == RESERVE) reserve_full(p);
+		else if(player_move == PURCHASE) purchase_full(p);
+		else if(player_move == PURCHASE_R){
+			int index;
+			index = sc.next();
+			purchase_reserve(p, index-1);
+		}	
+	
+	}
+
+	
+	public void purchase_full(Player p){
+		char card_type = sc.next();
+		char col_index = sc.next();
+		int y_pos = get_row(card_type);		
+		reserve(p,y_pos,col_index-1);
+	}
+	
+	public void reserve_full(Player p){
+		char card_type = sc.next();
+		col_index = sc.next();
+		int y_pos = get_row(card_type);
+		reserve(p, y_pos, col_index-1);
+	}
+
+
+	public void take_3_full (Player p){
+		String gem1 = sc.next();
+		String gem2 = sc.next();
+		String gem3 = sc.next();
+
+		String [] gems = {gem1, gem2, gem3};
+		for(int i; i<3; i++) take_3(p, gems[i]);
+}
+
+	
+	public int sum_gems(Player p){
+		int sum = 0;
+		for(int i = 0; i<6;i++){
+			sum += p.getGemCount(i);
+		}
+		return(sum);
+	}
+
+
+	public void over_10(Player p){
+		String gem_to_return = sc.next();
+		int return_index = gem_index(gem_to_return);
+		p.set_gem_count(-1, return_index);
+		this.gem_bank[return_index]++; 
+		clearBoard();
+		screen_clear();
+		screen_home();
 		
 
 	}
 
+	
+	public void 
 
 
 
